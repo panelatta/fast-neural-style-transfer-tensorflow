2db2df219a47068b9c42881c6ffc5d96222c59fe
panelatta <dennis.kuo@outlook.com>
2019-06-24 21:41:57 +0800

    rewrite all codes

--- README.md
+++ README.md
@@ -1,7 +1,61 @@
-# fast-neural-style-transfer-tensorflow
-An implementation for an Image Transformation Network described in [[Perceptual Losses for Real-Time Style Transfer and Super-Resolution,  Johnson et al.](https://arxiv.org/abs/1603.08155)](https://arxiv.org/abs/1603.08155), which was choosed to be my Python course homework, and also in an attempt to learn tensorflow.
+## tensorflow-slim-fast-style-transfer
+Implement of fast-style-transfer by Tensorflow-slim(Easy to read)
+## Requirements
+Tensorflow(>=1.4)+slim+python(2.7)
+## Dataset
+COCO2014 http://images.cocodataset.org/zips/train2014.zip
 
-Cause I'm just a newbie in the field of deep learning, and I'm also not skilled in coding with tensorflow yet, this implementation has referenced [OlavHN/fast-neural-style](https://github.com/OlavHN/fast-neural-style) and [hzy46/fast-neural-style-tensorflow](https://link.zhihu.com/?target=https%3A//github.com/hzy46/fast-neural-style-tensorflow) to a large extent. I'm really appreciated to them for their code does help me a lot.
+You need unzip it, and place all images in 'train2014' folder.
 
-The implementation is also based on [Tensorflow-Slim](https://github.com/tensorflow/models/tree/master/slim).
+All style images are provided by https://github.com/lengstrom/fast-style-transfer
+## Parameters
+All the parameters are the same with paper Perceptual Losses for Real-Time Style Transfer and Super-Resolution
+  
+STYLE_WEIGHT and CONTENT_WEIGHT are provided by https://github.com/hzy46/fast-neural-style-tensorflow 
+## Usage
+Please download slim vgg_16 check point from http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
 
+Then untar it and place it in 'model' folder.
+```Bash
+sudo ./convert_images_to_list.sh train2014/
+```
+to generate train images addresses list.
+```Python
+python train.py
+```
+It trains 'wave' style by default, you can change STYLE_IMAGE_PATH, TRAIN_CHECK_POINT and STYLE_WEIGHT in train.py to train other styles.
+
+You can use 
+```Bash
+Tensorboard --logdir=.
+```
+in log folder to see more training details.
+
+```Python
+python test.py
+```
+for testing.
+
+Model checkpoint I have trained: https://drive.google.com/drive/folders/1akw7PY6yF6A9hG_Au-j2U8-cDFfZ5uj8?usp=sharing
+
+Please place style folders in 'model/trained_model/', and modify MODEL_PATH in test.py to get your own style images. For example, if you want to get 'scream' style images, please place 'scream' folder in 'model/trained_model/', and change MODEL_PATH in test.py into 'model/trained_model/scream/model.ckpt-20001'. Then place your test.jpg in 'test' folder and python test.py.
+## Results
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/style_image.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/good_result.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/good_result2.jpg)
+
+Results with different steps.
+
+
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/scream_bown_result.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/scream_building.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/candy_bown.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/candy_building.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/starry_bown.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/starry_building.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/wave_bown.jpg)
+![](https://github.com/2012013382/tensorflow-slim-fast-style-transfer/blob/master/test/wave_building.jpg)
+## Reference
+https://github.com/hzy46/fast-neural-style-tensorflow
+
+https://github.com/lengstrom/fast-style-transfer
--- train.py
+++ train.py
@@ -1,16 +1,111 @@
+#coding: utf-8
 from __future__ import print_function
 from __future__ import division
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import model
+import data_process
+import numpy as np
+from os.path import join
+from scipy.misc import imsave
 import argparse
-import os
-import utils
 
-def main(confFile):
+# BATCH_SIZE = 4
+# IMAGE_SIZE = 256
+# CHANNEL_NUM = 3
+# CONTENT_LAYERS = ["vgg_16/conv3/conv3_3"]
+# STYLE_LAYERS = ["vgg_16/conv1/conv1_2", "vgg_16/conv2/conv2_2",
+#                 "vgg_16/conv3/conv3_3", "vgg_16/conv4/conv4_3"]
+
+# Style weight and content weight for predefined styles
+# PRESET_STYLES = {'candy' : (50.0, 1.0),
+#                  'cubist' : (180.0, 1.0),
+#                  'feathers' : (220.0, 1.0),
+#                  'mosaic' : (100.0, 1.0),
+#                  'scream' : (250.0, 1.0),
+#                  'udnie' : (200.0, 1.0),
+#                  'wave' : (220.0, 1.0)}
+# STYLE_WEIGHT = PRESET_STYLES['wave'][0]
+# CONTENT_WEIGHT = PRESET_STYLES['wave'][1]
+
+# MODEL_PATH = 'model/vgg_16.ckpt'
+# STYLE_IMAGE_PATH = 'style_images/wave.jpg'
+# TRAIN_IMAGE_PATH = 'train2014/'
+# TRAIN_CHECK_POINT = 'model/trained_model/wave/'
+# TEST_IMAGE_PATH = 'test/test.jpg'
+# IMAGE_SAVE_PATH = 'image/'
+# CHECK_POINT_PATH = 'log/'
+# LEARNING_RATE = 1e-3
+# EPOCH_NUM = 1
+# DATA_SIZE = 82783
+
+with tf.Graph().as_default():
+    image = tf.placeholder(tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, CHANNEL_NUM])
+    style_image = tf.placeholder(tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, CHANNEL_NUM])
+    #Obtain image features
+    generated_image = model.generator(image, training=True)
+    squeezed_generated_image = tf.image.encode_jpeg(tf.cast(tf.squeeze(generated_image, [0]), tf.uint8))
+    #Obtain loss model layers for image, generated_image and style image.
+    _, endpoints_mixed = model.loss_model(tf.concat([image, generated_image, style_image], 0))
+    variables_to_restore = slim.get_variables_to_restore(include=['vgg_16'])
+    restorer = tf.train.Saver(variables_to_restore)
+    #Content loss
+    content_loss = model.content_loss(endpoints_mixed, CONTENT_LAYERS)
+    #Style loss
+    style_loss = model.style_loss(endpoints_mixed, STYLE_LAYERS)
+
+    loss = STYLE_WEIGHT * style_loss + CONTENT_WEIGHT * content_loss
+    tf.summary.scalar('losses/content_loss', CONTENT_WEIGHT * content_loss)
+    tf.summary.scalar('losses/style_loss', STYLE_WEIGHT * style_loss)
+    tf.summary.scalar('losses/loss', loss)
+    tf.summary.image('generated', generated_image)
+    tf.summary.image('origin', image)    
+    summary = tf.summary.merge_all()
+	#Only train generator network
+    variables_for_training = slim.get_variables_to_restore(include=['generator'])
+    gradients = tf.gradients(loss, variables_for_training)
+    grad_and_var = list(zip(gradients, variables_for_training))
+
+    optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
+    opt_op = optimizer.apply_gradients(grads_and_vars=grad_and_var)
+
+    #Only save parameters of generator model
+    variables_to_save = slim.get_variables_to_restore(include=['generator'])
+    saver = tf.train.Saver(variables_to_save, max_to_keep=100)
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    
+    with tf.Session() as sess:
+        train_writer = tf.summary.FileWriter(CHECK_POINT_PATH, sess.graph)
+        # Restore variables from disk.
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        restorer.restore(sess, MODEL_PATH)
+        #Obtain style image batch
+        style_image_batch = data_process.read_style_image(STYLE_IMAGE_PATH, BATCH_SIZE)
+        step = 0
+		
+		img_paths = []
+        for epoch in range(EPOCH_NUM):
+            batch_index = 0
+            for i in range(DATA_SIZE // BATCH_SIZE):
+                image_batch, batch_index, img_paths = data_process.read_batches(TRAIN_IMAGE_PATH, img_paths, batch_index, BATCH_SIZE)
+                _, batch_ls, style_ls, content_ls, summary_str = sess.run([opt_op, loss, style_loss, content_loss, summary], feed_dict={image: image_batch,
+                                                                  style_image: style_image_batch})
+                step += 1
+                if i % 10 == 0:
+                    print('Epoch %d, Batch %d of %d, loss is %.3f, style loss is %.3f, content loss is %.3f'%(epoch + 1, i, DATA_SIZE // BATCH_SIZE, batch_ls, 220 * style_ls, content_ls))
+                    train_writer.add_summary(summary_str, step)
+                    test_image = data_process.read_test_image(TEST_IMAGE_PATH)
+                    styled_image = sess.run(squeezed_generated_image, feed_dict={image: test_image})
+                    #imsave(join(IMAGE_SAVE_PATH, 'epoch' + str(epoch + 1) + '.jpg'), styled_image)
+                    with open('training_image/res.jpg', 'wb') as img_s:
+                         img_s.write(styled_image)
+                if i % 1000 == 0:   
+            	    #save model parameters
+                    saver.save(sess, join(TRAIN_CHECK_POINT, 'model.ckpt'), global_step=step)
 
 
-if __name__ == '__main__':
-    # Reading parser from command line
-    # confFilePath: The path to the model config file
-    parser = argparse.ArgumentParser();
-    parser.add_argument('confFilePath', type=str, help='The path to the model config file')
-    main(utils.ReadConfFile(parser.confFilePath))
+
+
